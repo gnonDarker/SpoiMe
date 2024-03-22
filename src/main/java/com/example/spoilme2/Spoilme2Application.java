2ace2890bad43fce81d5b34212f4e5566ab98fb8
gnon <gnon2002@qq.com>
2024-03-22 15:39:48 +0800

    初始化

---
+++ src/main/java/com/example/spoilme2/Spoilme2Application.java
@@ -0,0 +1,13 @@
+package com.example.spoilme2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spoilme2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spoilme2Application.class, args);
+    }
+
+}
