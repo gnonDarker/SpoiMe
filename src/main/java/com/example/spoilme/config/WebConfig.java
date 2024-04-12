7a2c2d30fd67a6682f35151880fc962fd9e4d254
gnon <gnon2002@qq.com>
2024-04-13 00:36:51 +0800

    修改所有请求方式为Post

--- src/main/java/com/example/spoilme/config/WebConfig.java
+++ src/main/java/com/example/spoilme/config/WebConfig.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration //配置类
@@ -17,9 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        /*registry.addInterceptor(loginCheckInterceptor).addPathPatterns("/**").excludePathPatterns("/login")
-                .excludePathPatterns("/users")..excludePathPatterns("/register").excludePathPatterns("/photos/**");*/
-        //关闭拦截器便于测试
+        registry.addInterceptor(loginCheckInterceptor).addPathPatterns("/**").excludePathPatterns("/login")
+                .excludePathPatterns("/register");
     }
 
     //跨域配置
